bd8acffac56866baee5b7404170efdec9cdb1389
Hari Priya <haripriya816141@gmail.com>
2025-12-27 21:35:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let input = prompt("Enter a year");
+
+if (input === null || input.trim() === ""){
+	alert("Error: Please enter a valid year")
+}else{
+	let year = Number(input);
+	if(Number.isNaN(year)){
+		alert("Error: year must be a number");
+	}else{
+		alert(daysOfAYear(year));
+	}
+}
+
+function daysOfAYear(y){
+	if ((y % 4 === 0 && y % 100 !== 0) || y % 400 === 0){
+		return 366;
+	}else{
+		return 365;
+	}
+}
+
+
+
+
+
+
